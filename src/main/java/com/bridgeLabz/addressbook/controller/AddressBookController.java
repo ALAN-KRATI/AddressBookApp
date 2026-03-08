b8f7c113b426bb745d9ef705db7667a3175f2b20
ALAN-KRATI <alankratisaxena@gmail.com>
2026-03-08 14:55:31 +0530

    UC2: Ability to add new contact to AddressBook

--- src/main/java/com/bridgeLabz/addressbook/controller/AddressBookController.java
+++ src/main/java/com/bridgeLabz/addressbook/controller/AddressBookController.java
@@ -1,15 +1,26 @@
 package com.bridgeLabz.addressbook.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.*;
 import com.bridgeLabz.addressbook.model.Contact;
-
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/addressbook")
 public class AddressBookController {
+    private List<Contact> contacts = new ArrayList<>();
     
-    @PostMapping("/create")
-    public Contact createContact(@RequestBody Contact contact){
-       return contact;
+    @PostMapping("/add")
+    public String createContact(@RequestBody Contact contact){
+        contacts.add(contact);
+        return "Contact added to Address Book successfully";
     }
+
+    @GetMapping("/all")
+    public List<Contact> getAllContacts() {
+        return contacts;
+    }
+    
 }
