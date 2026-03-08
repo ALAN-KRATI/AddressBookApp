d972dee230b9131dba1fc90a7cd80c0318aaf98a
ALAN-KRATI <alankratisaxena@gmail.com>
2026-03-08 15:09:43 +0530

    UC3: Added ability to edit an existing contact by name

--- src/main/java/com/bridgeLabz/addressbook/controller/AddressBookController.java
+++ src/main/java/com/bridgeLabz/addressbook/controller/AddressBookController.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.*;
 import com.bridgeLabz.addressbook.model.Contact;
-import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 @RequestMapping("/addressbook")
@@ -21,6 +21,25 @@ public class AddressBookController {
     @GetMapping("/all")
     public List<Contact> getAllContacts() {
         return contacts;
+    }
+
+    @PutMapping("/edit/{firstName}")
+    public String editContact(@PathVariable String firstName, @RequestBody Contact contact) {
+        for(Contact c : contacts){
+            if(c.getFirstName().equalsIgnoreCase(firstName)){
+                c.setLastName(contact.getLastName());
+                c.setAddress(contact.getAddress());
+                c.setCity(contact.getCity());
+                c.setState(contact.getState());
+                c.setZip(contact.getZip());
+                c.setPhoneNumber(contact.getPhoneNumber());
+                c.setEmail(contact.getEmail());
+
+                return "Contact updated successfully";
+            }
+        }
+        
+        return "Contact not found";
     }
     
 }
